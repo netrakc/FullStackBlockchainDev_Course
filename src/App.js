32e6724bb0f105f8648105defeaa7f578ebaf03a
Netra Bahadur khatri <110593103+netrakc@users.noreply.github.com>
2022-12-31 07:59:19 +0000

    first react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './component/netra';
+
+function App() {
+  return (
+    <div className="App">
+      {/* MyComponent Function is called over here..! */}
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/netra.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Functional component.
+const MyComponent = () => {
+    return <h1>Hello, React is best for UI development.</h1>
+
+}
+
+export default MyComponent
+
